7f40731fe61d97d6923e664713b27473b9b2d3f1
nhbala <nb378@cornell.edu>
2020-05-26 17:19:08 -0400

    persistence for adding categories

--- content.js
+++ content.js
@@ -1,36 +1,76 @@
+//When content is first loaded function call
 document.addEventListener('DOMContentLoaded', function() {
 
-
+  // localStorage.clear();
+  //Adding in tabs on start up? (do we need to include first tab could be removed later)
   chrome.windows.getAll({populate:true},function(windows){
-windows.forEach(function(window){
-  window.tabs.forEach(function(tab){
-    var currElement = document.getElementById(tab.id)
+    windows.forEach(function(window){
+      window.tabs.forEach(function(tab){
+        var currElement = document.getElementById(tab.id)
 
-      console.log(tab.url);
-      console.log(tab.id)
-      let currTabId = tab.id
-      var ul = document.getElementById("myUL");
-      var li = document.createElement("li");
-      var a = document.createElement("button");
-      a.textContent = tab.title;
-      a.setAttribute("id", tab.id);
-      a.addEventListener('click', function(){
-        console.log("hixs");
-        chrome.windows.update(tab.windowId, {focused: true});
-        chrome.tabs.update(tab.id, {selected: true});
-
-      })
-      li.appendChild(a);
-      li.setAttribute("id", "li" + currTabId.toString()); // added line
-      ul.appendChild(li);
+          let currTabId = tab.id
+          var ul = document.getElementById("myUL");
+          var li = document.createElement("li");
+          var a = document.createElement("button");
+          a.textContent = tab.title;
+          a.setAttribute("id", tab.id);
+          a.addEventListener('click', function(){
+            chrome.windows.update(tab.windowId, {focused: true});
+            chrome.tabs.update(tab.id, {selected: true});
+          })
+          li.appendChild(a);
+          li.setAttribute("id", "li" + currTabId.toString()); // added line
+          ul.appendChild(li);
+        });
+      });
     });
-  });
-});
+
+    //Keeping category titles
+    let currNumberDivs = localStorage.getItem("categoryNum");
+    if (currNumberDivs != null){
+      for (i = 1; i <= currNumberDivs; i++){
+        let categoryTitle = JSON.parse(localStorage.getItem(i))["name"];
+        let categoryDiv = document.getElementById("categoryList");
+        let newDiv = document.createElement("div");
+        newDiv.textContent = categoryTitle;
+        categoryDiv.appendChild(newDiv);
+      }
+    }
 
 
+
+
+    var createButton = document.getElementById('createButton');
+    createButton.addEventListener('click', function(){
+      if (localStorage.getItem("categoryNum") === null) {
+        localStorage.setItem("categoryNum", 1);
+      }else{
+        //increase number for categoryNum
+        let curr_num = parseInt(localStorage.getItem("categoryNum"),10) + 1;
+        localStorage.setItem("categoryNum", curr_num);
+      }
+      let categoryTitle = document.getElementById("newCategoryText").value;
+      let categoryDiv = document.getElementById("categoryList");
+      let newDiv = document.createElement("div");
+      newDiv.textContent = categoryTitle;
+      newDiv.setAttribute("id", "categoryId" + localStorage.getItem("categoryNum").toString());
+      categoryDiv.appendChild(newDiv);
+      let divDict = {};
+      divDict["name"] = categoryTitle;
+      divDict["tab_ids"] = [];
+      localStorage.setItem(localStorage.getItem("categoryNum"), JSON.stringify(divDict));
+
+
+
+
+
+    })
+
+
+
+    //Creating search Bar logic (change later?)
     var link = document.getElementById('searchBar');
     link.addEventListener('keydown', function() {
-
           var input, filter, ul, li, a, i, txtValue;
           input = document.getElementById("searchBar");
           filter = input.value.toUpperCase();
@@ -38,7 +78,6 @@ windows.forEach(function(window){
           li = ul.getElementsByTagName("li");
           for (i = 0; i < li.length; i++) {
               a = li[i].getElementsByTagName("button")[0];
-              console.log(a)
               txtValue = a.textContent || a.innerText;
               if (txtValue.toUpperCase().indexOf(filter) > -1) {
                   li[i].style.display = "";
@@ -49,7 +88,7 @@ windows.forEach(function(window){
     });
 });
 
-
+// When a tab is closed/window is closed
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
   var currElement = document.getElementById("li" + tabId)
   currElement.remove()
@@ -57,18 +96,22 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
 });
 
 
+var clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', function(){
+  localStorage.clear();
+});
 
+
+//When tabs are added/updated
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
 
+    //Checking all the current tabs open
     chrome.windows.getAll({populate:true},function(windows){
   windows.forEach(function(window){
     window.tabs.forEach(function(tab){
       var currElement = document.getElementById(tab.id)
       if (currElement == null){
-        console.log(tab.url);
-        console.log(tab.id)
-        console.log(tab.windowId)
         let currTabId = tab.id
         var ul = document.getElementById("myUL");
         var li = document.createElement("li");
@@ -76,11 +119,8 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
         a.textContent = tab.title;
         a.setAttribute("id", tab.id);
         a.addEventListener('click', function(){
-          console.log("hixs");
           chrome.windows.update(tab.windowId, {focused: true});
           chrome.tabs.update(tab.id, {selected: true});
-          let i = chrome.windows.get(tab.windowId);
-          console.log(i.focused)
         })
         li.appendChild(a);
         li.setAttribute("id", "li" + currTabId.toString()); // added line
