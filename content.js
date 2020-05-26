2c1a72a54bcc4ebff81430e673a2e4ac881e7a29
nhbala <nb378@cornell.edu>
2020-05-25 22:52:07 -0400

    oops fixed search bar again

--- content.js
+++ content.js
@@ -28,7 +28,8 @@ document.addEventListener('DOMContentLoaded', function() {
           ul = document.getElementById("myUL");
           li = ul.getElementsByTagName("li");
           for (i = 0; i < li.length; i++) {
-              a = li[i].getElementsByTagName("a")[0];
+              a = li[i].getElementsByTagName("button")[0];
+              console.log(a)
               txtValue = a.textContent || a.innerText;
               if (txtValue.toUpperCase().indexOf(filter) > -1) {
                   li[i].style.display = "";
